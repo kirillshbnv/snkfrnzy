0f35127aadfb16d6ce737f7aec693187f92093bd
kirillshbnv <kshab.dev@gmail.com>
2025-05-13 23:54:04 -0600

    feat(globals): add direction constants

--- include/globals.h
+++ include/globals.h
@@ -2,6 +2,12 @@
 #define FALSE 0
 #define TRUE 1
 
+/* Direction */
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
 /* Types */
 typedef unsigned char UINT8;
 typedef unsigned int UINT16;
