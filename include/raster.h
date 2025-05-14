9ff3c64bca0842704ebbcb8d4e1228469ed1adcf
kirillshbnv <kshab.dev@gmail.com>
2025-05-13 23:51:48 -0600

    feat(raster): add public function declarations

---
+++ include/raster.h
@@ -0,0 +1,19 @@
+#ifndef RASTER_H
+#define RASTER_H
+
+#include "globals.h"
+
+
+void clear_screen(UINT8 screen);
+
+void plot_border(UINT8 screen);
+
+void plot_snake_head(UINT8 screen, int x, int y, UINT8 direction);
+
+void plot_snake_body(UINT8 screen, int x, int y, UINT8 direction);
+
+void plot_bug(UINT8 screen, int x, int y);
+
+void plot_spike(UINT8 screen, int x, int y);
+
+#endif /* RASTER_H */
