c6801614304e655eae1a6df346ea3f7e98ad32e3
kirillshbnv <kshab.dev@gmail.com>
2025-05-14 13:48:38 -0600

    refactor(raster): update screen parameter to pointer

--- src/raster.c
+++ src/raster.c
@@ -206,50 +206,50 @@ UINT16 border_segment[BORDER_SEG_HEIGHT] = {
 };
 
 
-void plot_border_seg(UINT8 screen, int x, int y);
+void plot_border_seg(UINT8 *screen, int x, int y);
 
-void plot_bitmap_8x8(UINT8 screen, int x, int y, UINT8 bitmap_8x8);
+void plot_bitmap_8x8(UINT8 *screen, int x, int y, UINT8 bitmap_8x8);
 
-void plot_bitmap_16x16(UINT8 screen, int x, int y, UINT16 bitmap_16x16);
+void plot_bitmap_16x16(UINT8 *screen, int x, int y, UINT16 bitmap_16x16);
 
 
 /************************/
 /* HIGH-LEVEL FUNCTIONS */
 /************************/
 
-void clear_screen(UINT8 screen) {
+void clear_screen(UINT8 *screen) {
 
 }
 
-void plot_pixel(UINT8 screen, int x, int y) {
+void plot_pixel(UINT8 *screen, int x, int y) {
 
 }
 
-void plot_vert_line(UINT8 screen, int x, int y1, int y2) {
+void plot_vert_line(UINT8 *screen, int x, int y1, int y2) {
 
 }
 
-void plot_horiz_line(UINT8 screen, int x1, int x2, int y) {
+void plot_horiz_line(UINT8 *screen, int x1, int x2, int y) {
 
 }
 
-void plot_border(UINT8 screen) {
+void plot_border(UINT8 *screen) {
 
 }
 
-void plot_snake_head(UINT8 screen, int x, int y, UINT8 direction) {
+void plot_snake_head(UINT8 *screen, int x, int y, UINT8 direction) {
 
 }
 
-void plot_snake_body(UINT8 screen, int x, int y, UINT8 direction) {
+void plot_snake_body(UINT8 *screen, int x, int y, UINT8 direction) {
 
 }
 
-void plot_bug(UINT8 screen, int x, int y) {
+void plot_bug(UINT8 *screen, int x, int y) {
 
 }
 
-void plot_spike(UINT8 screen, int x, int y) {
+void plot_spike(UINT8 *screen, int x, int y) {
 
 }
 
@@ -258,15 +258,15 @@ void plot_spike(UINT8 screen, int x, int y) {
 /* MID-LEVEL FUNCTIONS*/
 /**********************/
 
-void plot_border_seg(UINT8 screen, int x, int y) {
+void plot_border_seg(UINT8 *screen, int x, int y) {
 
 }
 
-void plot_bitmap_8x8(UINT8 screen, int x, int y, UINT8 bitmap_8x8) {
+void plot_bitmap_8x8(UINT8 *screen, int x, int y, UINT8 bitmap_8x8) {
 
 }
 
-void plot_bitmap_16x16(UINT8 screen, int x, int y, UINT16 bitmap_16x16) {
+void plot_bitmap_16x16(UINT8 *screen, int x, int y, UINT16 bitmap_16x16) {
 
 }
 
