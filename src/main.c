35048cd51864517a7ed95a3f71215604263d2ca5
kirillshbnv <kshab.dev@gmail.com>
2025-05-14 22:51:24 -0600

    chore(main): add temporary test code to verify build setup

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
-
+#include "../include/raster.h"
+#include <stdio.h>
+#include <osbind.h>
 
 int main() {
+    UINT8 *screen = Physbase();
+    clear_screen(screen);
+    printf("Hello World\n");
     return 0;
 }
